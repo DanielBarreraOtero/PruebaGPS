e82a19306bc5be6ccfd33d74d6571518e78e64e7
DanielBarreraOtero <dbarote0812@g.educaand.es>
2023-02-14 10:04:32 +0100

    Hecho formulario de registro

--- src/Entity/Participante.php
+++ src/Entity/Participante.php
@@ -7,10 +7,12 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: ParticipanteRepository::class)]
+#[UniqueEntity(fields: ['indicativo'], message: 'Ya existe una cuenta con este indicativo')]
 class Participante implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participante;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('indicativo', TextType::class, [
+                'constraints' => [
+                    new Length([
+                        'max' => 6,
+                        'maxMessage' => 'El indicativo debe tener un máximo de 6 caracteres.'
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[A-Z]{1,2}\d[A-Z]{1,3}$/',
+                        'match' => true,
+                        'message' => 'El formato del indicativo no es el adecuado.'
+                    ])
+                ]
+            ])
+            ->add('email', EmailType::class)
+            ->add('nombre')
+            ->add('apellido1')
+            ->add('apellido2')
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'You should agree to our terms.',
+            //         ]),
+            //     ],
+            // ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Por favor, introduzca una contraseña',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Su contraseña debe tener al menos {{ limit }} caracteres',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participante::class,
+        ]);
+    }
+}
